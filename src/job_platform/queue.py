62a8c94b7f22d5f994304124c6f7b6e13dd33833
Ebubekir <ebubekirpulat15@gmail.com>
2026-09-14 20:37:39 +0930

    Add Processing queue, and complete() in queue

--- src/job_platform/queue.py
+++ src/job_platform/queue.py
@@ -2,6 +2,7 @@ import uuid
 import redis
 
 QUEUE_NAME = "jobs:queue"
+PROCESSING_QUEUE_NAME = "jobs:processing"
 
 class JobQueue:
     def __init__(self):
@@ -16,5 +17,18 @@ class JobQueue:
         self.redis.rpush(QUEUE_NAME, str(job_id))
 
     def dequeue(self) -> uuid.UUID:
-        _, job_id = self.redis.blpop(QUEUE_NAME, timeout=0)
+        job_id = self.redis.blmove(
+            first_list=QUEUE_NAME, 
+            second_list=PROCESSING_QUEUE_NAME, 
+            timeout=0,
+            src="RIGHT",
+            dest="LEFT",
+        )
         return uuid.UUID(job_id)
+
+    def complete(self, job_id: uuid.UUID) -> None:
+        self.redis.lrem(
+            PROCESSING_QUEUE_NAME,
+            1,
+            str(job_id),
+        )
